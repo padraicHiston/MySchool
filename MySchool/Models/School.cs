af7f1915a43f6dd6371692d956232e9782a33740
Padraic <padraic.histon@gmail.com>
2017-05-04 13:21:40 +0100

    Added School model.

---
+++ MySchool/Models/School.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MySchool.Models
+{
+    public class School
+    {
+        [Required]
+        public string RollNumber { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string County { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public List<Teacher> Teachers { get; set; }
+        public List<Pupil> Pupils { get; set; }
+        public List<Parent> Parents { get; set; }
+        public int NumberOfBoys { get; set; }
+        public int NumberOfGirls { get; set; }
+        public int TotalPupilPopulation { get; set; }
+        public int NumberOfTeachers { get; set; }
+    }
+}
