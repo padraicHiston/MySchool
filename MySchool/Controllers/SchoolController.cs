f7e699b24df7f7085a78c5fabe68eb9ae885834d
Padraic <padraic.histon@gmail.com>
2017-05-04 14:29:57 +0100

    Added County model and School controller.

---
+++ MySchool/Controllers/SchoolController.cs
@@ -0,0 +1,26 @@
+﻿using MySchool.Models;
+using System.Web.Mvc;
+
+namespace MySchool.Controllers
+{
+    public class SchoolController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public SchoolController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: School
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MySchool/Models/County.cs
@@ -0,0 +1,8 @@
+﻿namespace MySchool.Models
+{
+    public class County
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
